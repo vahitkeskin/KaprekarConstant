379891be65acfc7b506538cabf833289d4e0f1b3
Vahit Keskin <vhtkskn63@gmail.com>
2026-08-07 21:39:45 +0300

    fix: Resolve Android app launcher icon visibility by generating true PNGs, removing overriding mipmap-anydpi XMLs, and setting transparent navigation bar

--- androidApp/src/main/kotlin/com/vahitkeskin/kaprekarconstant/MainActivity.kt
+++ androidApp/src/main/kotlin/com/vahitkeskin/kaprekarconstant/MainActivity.kt
@@ -1,15 +1,28 @@
 package com.vahitkeskin.kaprekarconstant
 
+import android.graphics.Color
 import android.os.Bundle
 import androidx.activity.ComponentActivity
+import androidx.activity.SystemBarStyle
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.core.view.WindowCompat
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
-        enableEdgeToEdge()
+        WindowCompat.setDecorFitsSystemWindows(window, false)
+        enableEdgeToEdge(
+            statusBarStyle = SystemBarStyle.auto(
+                Color.TRANSPARENT,
+                Color.TRANSPARENT
+            ),
+            navigationBarStyle = SystemBarStyle.auto(
+                Color.TRANSPARENT,
+                Color.TRANSPARENT
+            )
+        )
         super.onCreate(savedInstanceState)
 
         setContent {
